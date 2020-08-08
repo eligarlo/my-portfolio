83d5baa83572c6d4a279d60c552dcb65584b8b13
eligarlo <eligarlo.developer@gmail.com>
2020-08-08 21:54:43 +0300

    import fotawesome icons

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// FontAwesome Setup Start
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTwitter, faGithub, faLinkedin)
+Vue.component('FontAwesome', FontAwesomeIcon)
+// FontAwesome Setup End
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
