8c8f909b36f794f48bc734baaa9858498643184f
eligarlo <eligarlo.developer@gmail.com>
2020-08-09 21:03:02 +0300

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,10 +5,18 @@
 				Hi there,
 				<span class="text-secondary">Eliahu</span> here!
 			</h2>
-			<p>A few years ago I started to program in a self-taught way and I never thought that, to my almost 30 years then, I was going to find a hobby to devote myself with so much passion. This led me to study a Full Stack professional course at a renowned academy in Israel, as well as to buy/learn other courses on platforms like Udemy looking for the most complete training possible.</p>
+
+			<div class="flex-container">
+				<img :src="require(`@/assets/img/godella.svg`)" alt="Godella Map" />
+				<p>A few years ago I started to program in a self-taught way and I never thought that, to my almost 30 years then, I was going to find a hobby to devote myself with so much passion. This led me to study a Full Stack professional course at a renowned academy in Israel, as well as to buy/learn other courses on platforms like Udemy looking for the most complete training possible.</p>
+			</div>
+
+			<div class="felx-container-reverse"></div>
+
+			<!-- <p>A few years ago I started to program in a self-taught way and I never thought that, to my almost 30 years then, I was going to find a hobby to devote myself with so much passion. This led me to study a Full Stack professional course at a renowned academy in Israel, as well as to buy/learn other courses on platforms like Udemy looking for the most complete training possible.</p>
 			<p>I like to know that being a programmer is a career that is going to keep this hobby active all my life since all the time there are new technologies to learn.</p>
 			<p>I am a person committed and always dedicated to his work and above all to his passion which I think is the key to success.</p>
-			<p>I have had the opportunity to develop projects both alone and with a team. But without a doubt, the team is the best option. In a team one not only works but also learns and shares knowledge, something that for me is essential if you want a job well done and move forward.</p>
+			<p>I have had the opportunity to develop projects both alone and with a team. But without a doubt, the team is the best option. In a team one not only works but also learns and shares knowledge, something that for me is essential if you want a job well done and move forward.</p>-->
 		</div>
 	</main>
 </template>
